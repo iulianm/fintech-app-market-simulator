21af6950fe9a60984b7b1432a25a98470a046e4e
Iulian <iulian.moisa@gmail.com>
2018-12-28 20:03:03 +0000

    first commit

---
+++ src/main/java/com/fintech/marketsimulator/MarketSimulatorApplication.java
@@ -0,0 +1,14 @@
+package com.fintech.marketsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketSimulatorApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/fintech/marketsimulator/MarketSimulatorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.fintech.marketsimulator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarketSimulatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
